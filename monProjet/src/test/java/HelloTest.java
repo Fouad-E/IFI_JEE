eebc91dbc57d941f864604492909857af8380daf
El Ati Fouad <elati@b11p19.fil.univ-lille1.fr>
2018-11-05 09:11:46 +0100

    Projects added

---
+++ monProjet/src/test/java/HelloTest.java
@@ -0,0 +1,15 @@
+
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class HelloTest{
+
+    @Test
+    public void testGetMessage(){
+        assertEquals("Hello World", new Hello().getMessage());
+    }
+
+}
+
+
